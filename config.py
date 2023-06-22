a2ededed40790584c5dc1bb67f18668b68dbb7f6
Alyokha2022 <99477721+Alyokha2022@users.noreply.github.com>
2023-06-22 13:15:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-comunity_token = 
+comunity_token = vk1.a.OZHohotXr8pSCeYq_b59FRx_7DSgi4O8-nrQ0jLLYNaEeGueEGV5-WJPwVrsjmAT8cKGzxSdPstXatfeZ81Zfrm-kxkt3wzFdDIszmibQ_04yW5wVp5UgGbKftb8vV6Y25V4lW2AH50fySO7pXH7KyfoXgjtiplKx8GxmDablVE5_Y-oqSV8SW7t0c9KBq8zJXyLP1qMKdMkXECHP4TWrA
 acces_token = 
 
 db_url_object = "postgresql+psycopg2://postgres:180186@localhost:5432/diplom"
